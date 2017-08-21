df393401be0db15a307f8da03b105e66cf2e6077
Harris <25195237@qq.com>
2017-08-20 23:54:07 -0700

    addSaveFunction
    add save asset function, with wip files and comments, rename the scripts to workspace and files, try to be more orgnazied. haven't test the function yet

---
+++ maya/common/files.py
@@ -0,0 +1,32 @@
+## External Import
+import json
+
+
+#### Functions
+def writeJsonFile(sPath, data):
+	with open(sPath, 'w') as sOutfile:
+		json.dump(data, sOutfile)
+	file.close(sOutfile)
+
+def readJsonFile(sPath):
+	if not os.path.exists(sPath):
+		raise RuntimeError('The file is not exist')
+	with open(sPath, 'r') as sInfile:
+		data = json.loads(sInfile)
+	file.close(sInfile)
+	return data
+
+#### sub Functions
+def _convertStringToCamelcase(sString):
+	if '_' in sString:
+		sStringParts = sString.split('_')
+		sString = ''
+		for sPart in sStringParts:
+			sString += sPart[0].upper() + sPart[1:]
+	if ' ' in sString:
+		sStringParts = sString.split(' ')
+		sString = ''
+		for sPart in sStringParts:
+			sString += sPart[0].upper() + sPart[1:]
+	sString = sString[0].lower() + sString[1:]
+	return sString
