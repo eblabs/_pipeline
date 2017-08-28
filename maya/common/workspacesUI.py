fa64d86decb4f2d3ef1709eb5ea7c6073973f236
Harris <25195237@qq.com>
2017-08-27 22:57:35 -0700

    listAssets
    assets will be listed if select a project, also the right click rename/delete will be disabled if nothing selected

--- maya/common/workspacesUI.py
+++ maya/common/workspacesUI.py
@@ -37,15 +37,58 @@ class assetsManagerUI(QtGui.QWidget):
 		self.setLayout(self.QLayoutBase)
 
 		## Project
-		QLayout_projectList, QSourceModel_project, self.QProxyModel_project, self.QFilterEdit_project = self.setBaseLayout('Projects')
+		QLayout_projectList, QSourceModel_project, self.QProxyModel_project, self.QFilterEdit_project, self.QListView_project = self._setBaseLayout('Projects')
 		self.rebuildListModel(QSourceModel_project, workspaces.sPathLocal, sType = 'folder')
-		self.QFilterEdit_project.textChanged.connect(self.filterRegExpChanged_project)
-
+		self.QFilterEdit_project.textChanged.connect(self._filterRegExpChanged_project)
+		self.QListView_project.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+		self.QListView_project.connect(self.QListView_project, QtCore.SIGNAL("customContextMenuRequested(QPoint)" ), self.listItemRightClicked_project)
 		## Asset
-		QLayout_assetList, QSourceModel_asset, self.QProxyModel_asset, self.QFilterEdit_asset = self.setBaseLayout('Assets')
+		QLayout_assetList, self.QSourceModel_asset, self.QProxyModel_asset, self.QFilterEdit_asset, self.QListView_asset = self._setBaseLayout('Assets')
+		QSelectionModel_asset = self.QListView_project.selectionModel()
+		QSelectionModel_asset.currentChanged.connect(self.listItemClick_asset)
+		self.QFilterEdit_asset.textChanged.connect(self._filterRegExpChanged_asset)
 
+	def rebuildListModel(self, QModel, sPath, sType = 'folder'):
+		QModel.clear()
+		lFiles = files.getFilesFromPath(sPath, sType = sType)
+		if len(lFiles) > 1: lFiles.sort()
+		for sFile in lFiles:
+			sFile_item = QtGui.QStandardItem(sFile)
+			QModel.appendRow(sFile_item)
 
-	def setBaseLayout(self, sName, bFilter = True):
+	def listItemClick_asset(self):
+		currentItem = self.QListView_project.currentIndex().data()
+		if currentItem:
+			sPath = os.path.join(workspaces.sPathLocal, str(currentItem))
+			sPath = os.path.join(sPath, 'assets')
+			if os.path.exists(sPath):
+				self.rebuildListModel(self.QSourceModel_asset, sPath)
+			else:
+				self.QSourceModel_asset.clear()
+		else:
+			self.QSourceModel_asset.clear()
+
+	def listItemRightClicked_project(self, QPos):
+		QListMenu= QtGui.QMenu(self)
+		QMenuItem_create = QListMenu.addAction("Create")
+		QMenuItem_rename = QListMenu.addAction("Rename")
+		QMenuItem_delete = QListMenu.addAction("Delete")
+		#self.connect(menu_item_rename, QtCore.SIGNAL("triggered()"), self.renameItem) 
+		#self.connect(menu_item_delete, QtCore.SIGNAL("triggered()"), self.menuItemClicked) 
+
+		parentPosition = self.QListView_project.mapToGlobal(QtCore.QPoint(0, 0))        
+		QListMenu.move(parentPosition + QPos)
+
+		try:
+			currentItem = self.QListView_project.currentIndex().data()
+			if not currentItem:
+				QMenuItem_rename.setEnabled(False)
+				QMenuItem_delete.setEnabled(False)			
+			QListMenu.show() 
+		except:
+			pass
+
+	def _setBaseLayout(self, sName, bFilter = True):
 		#Layout
 		QLayout = QtGui.QVBoxLayout()
 		self.QLayoutBase.addLayout(QLayout)
@@ -72,30 +115,44 @@ class assetsManagerUI(QtGui.QWidget):
 		else:
 			QProxyModel = None
 
-		QListView = QtGui.QListView()
+		#QListView = QtGui.QListView()
+		QListView = fileListView()
 		if bFilter:
 			QListView.setModel(QProxyModel)
 		else:
 			QListView.setModel(QSourceModel)
 		QLayout_list.addWidget(QListView)
 
-		return QLayout_list, QSourceModel, QProxyModel, QFilterEdit
+		return QLayout_list, QSourceModel, QProxyModel, QFilterEdit, QListView
 
-	def rebuildListModel(self, QModel, sPath, sType = 'folder'):
-		lFiles = files.getFilesFromPath(sPath, sType = sType)
-		if len(lFiles) > 1: lFiles.sort()
-		for sFile in lFiles:
-			sFile_item = QtGui.QStandardItem(sFile)
-			QModel.appendRow(sFile_item)
-
-	def filterRegExpChanged_project(self):
+	def _filterRegExpChanged_project(self):
 		regExp = QtCore.QRegExp(self.QFilterEdit_project.text(), QtCore.Qt.CaseInsensitive)
 		self.QProxyModel_project.setFilterRegExp(regExp)
 
+	def _filterRegExpChanged_asset(self):
+		regExp = QtCore.QRegExp(self.QFilterEdit_asset.text(), QtCore.Qt.CaseInsensitive)
+		self.QProxyModel_asset.setFilterRegExp(regExp)
+
 ## List widget
 class fileListView(QtGui.QListView):
-	def __init__(self, type, parent=None):
+	def __init__(self, parent=None):
 		super(fileListView, self).__init__(parent)
+
+	def keyPressEvent(self, event):
+		if (event.key() == QtCore.Qt.Key_Escape and
+			event.modifiers() == QtCore.Qt.NoModifier):
+			self.clearSelection()
+			self.clearFocus()
+			self.setCurrentIndex(QtCore.QModelIndex())
+		else:
+			QtGui.QListView.keyPressEvent(self, event)
+
+	def mousePressEvent(self, event):
+		if not self.indexAt(event.pos()).isValid():
+			self.clearSelection()
+			self.clearFocus()
+			self.setCurrentIndex(QtCore.QModelIndex())
+		QtGui.QListView.mousePressEvent(self, event)
 
 ## Functions
 def getMayaWindow():
