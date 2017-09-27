00150e59eb533a93441ee7a04545f00f74e75cac
Harris <25195237@qq.com>
2017-09-27 08:58:36 -0700

    namingAPI
    finish namingAPI for now

--- maya/namingAPI/naming.py
+++ maya/namingAPI/naming.py
@@ -4,16 +4,9 @@ class oName(object):
 	"""docstring for oName"""
 	def __init__(self, *args, **kwargs):
 		super(oName, self).__init__()
-		## arguments
-		self.sType = None
-		self.sSide = None
-		self.sRes = None
-		self.sPart = None
-		self.iIndex = None
-		self.iSuffix = None
 
 		if args:
-			self.decomposeName(args[0])
+			self._decomposeName(args[0])
 		else:
 			sType = kwargs.get('sType', None)
 			sSide = kwargs.get('sSide', None)
@@ -23,11 +16,11 @@ class oName(object):
 			iSuffix = kwargs.get('iSuffix', None)
 
 			if sType:
-				self.sType = getKeyFromNamePart(sType, 'type')
+				self._sType = sType
 			if sSide:
-				self.sSide = getKeyFromNamePart(sSide, 'side')
+				self._sSide = sSide
 			if sRes:
-				self.sRes = getKeyFromNamePart(sRes, 'resolution')
+				self._sRes = sRes
 			if sPart:
 				self.sPart = sPart
 			if iIndex:
@@ -36,17 +29,71 @@ class oName(object):
 				self.iSuffix = iSuffix
 
 	@property
+	def sType(self):
+		return self._sType
+
+	@property
+	def sSide(self):
+		return self._sSide
+
+	@property
+	def sRes(self):
+		return self._sRes
+
+	@sType.setter
+	def sType(self, sKey):
+		if sKey:
+			sName = getKeyFromNamePart(sKey, 'type')
+		else:
+			sName = None
+		self._sType = sName
+
+	@sSide.setter
+	def sSide(self, sKey):
+		if sKey:
+			sName = getKeyFromNamePart(sKey, 'side')
+		else:
+			sName = None
+		self._sSide = sName
+
+	@sRes.setter
+	def sRes(self, sKey):
+		if sKey:
+			sName = getKeyFromNamePart(sKey, 'resolution')
+		else:
+			sName = None
+		self._sRes = sName
+
+	@property
+	def sTypeKey(self):
+		if self._sType:
+			sKey = getFullNameFromKey(self._sType, 'type')
+		else:
+			sKey = None
+		return sKey
+
+	@property
+	def sSideKey(self):
+		if self._sSide:
+			sKey = getFullNameFromKey(self._sSide, 'side')
+		else:
+			sKey = None
+		return sKey
+
+	@property
+	def sResKey(self):
+		if self._sRes:
+			sKey = getFullNameFromKey(self._sRes, 'resolution')
+		else:
+			sKey = None
+		return sKey
+
+	@property
 	def sName(self):
-		if not self.sPart or not self.sType:
+		if not self.sPart or not self._sType:
 			raise RuntimeError('The name entered is invalid')
-		if sType:
-			self.sType = getKeyFromNamePart(sType, 'type')
-		if sSide:
-			self.sSide = getKeyFromNamePart(sSide, 'side')
-		if sRes:
-			self.sRes = getKeyFromNamePart(sRes, 'resolution')
 		sName = ''
-		for sNamePart in [self.sType, self.sSide, self.sRes, self.sPart]:
+		for sNamePart in [self._sType, self._sSide, self._sRes, self.sPart]:
 			if sNamePart:
 				sName += '%s_' %sNamePart
 		for iNum in [self.iIndex, self.iSuffix]:
@@ -54,72 +101,54 @@ class oName(object):
 				sName += '%03d_' %iNum
 		return sName[:-1]
 
-	def decomposeName(self, sName):
+	def _decomposeName(self, sName):
 		lNameParts = sName.split('_')
-		if len(lNameParts) == 6:
-			sType = getKeyFromNamePart(lNameParts[0], 'type')
-			sSide = getKeyFromNamePart(lNameParts[1], 'side')
-			sRes = getKeyFromNamePart(lNameParts[2], 'resolution')
-			sPart = lNameParts[3]
-			iIndex = int(lNameParts[4])
-			iSuffix = int(lNameParts[5])
+		self._sType = getKeyFromNamePart(lNameParts[0], 'type')
+		if len(lNameParts) == 6:			
+			self._sSide = getKeyFromNamePart(lNameParts[1], 'side')
+			self._sRes = getKeyFromNamePart(lNameParts[2], 'resolution')
+			self.sPart = lNameParts[3]
+			self.iIndex = int(lNameParts[4])
+			self.iSuffix = int(lNameParts[5])
 		elif len(lNameParts) == 5:
-			sType = getKeyFromNamePart(lNameParts[0], 'type')
-
 			if lNameParts[4].isdigit() and lNameParts[3].isdigit():
-				iIndex = int(lNameParts[3])
-				iSuffix = int(lNameParts[4])
-				sPart = lNameParts[2]
-				sSide = getKeyFromNamePart(lNameParts[1], 'side')
-				if sSide:
-					sRes = None 
-					sResValue = None
+				self.iIndex = int(lNameParts[3])
+				self.iSuffix = int(lNameParts[4])
+				self.sPart = lNameParts[2]
+				self._sSide = getKeyFromNamePart(lNameParts[1], 'side')
+				if self.sSide:
+					self._sRes = None 
 				else:
-					sRes = getKeyFromNamePart(lNameParts[1], 'resolution')
+					self._sRes = getKeyFromNamePart(lNameParts[1], 'resolution')
 			else:
-				iIndex = int(lNameParts[4])
-				sPart = lNameParts[3]
-				sSide = getKeyFromNamePart(lNameParts[1], 'side')
-				sRes = getKeyFromNamePart(lNameParts[2], 'resolution')
-				iSuffix = None
+				self.iIndex = int(lNameParts[4])
+				self.sPart = lNameParts[3]
+				self._sSide = getKeyFromNamePart(lNameParts[1], 'side')
+				self._sRes = getKeyFromNamePart(lNameParts[2], 'resolution')
+				self.iSuffix = None
 
 		elif len(lNameParts) == 4:
-			sType = getKeyFromNamePart(lNameParts[0], 'type')
-			sSide = getKeyFromNamePart(lNameParts[1], 'side')
-			iSuffix = None
+			self._sSide = getKeyFromNamePart(lNameParts[1], 'side')
+			self.iSuffix = None
 			if lNameParts[3].isdigit():
-				iIndex = int(lNameParts[3])
-				sPart = lNameParts[2]
-				sRes = None
-				sResValue = None
+				self.iIndex = int(lNameParts[3])
+				self.sPart = lNameParts[2]
+				self._sRes = None
 			else:
-				sSide = getKeyFromNamePart(lNameParts[1], 'side')
-				sRes = getKeyFromNamePart(lNameParts[2], 'resolution')
-				sPart = lNameParts[3]
-				iIndex = None
+				self._sSide = getKeyFromNamePart(lNameParts[1], 'side')
+				self._sRes = getKeyFromNamePart(lNameParts[2], 'resolution')
+				self.sPart = lNameParts[3]
+				self.iIndex = None
 
 		else:
-			sType = getKeyFromNamePart(lNameParts[0], 'type')
-			sPart = lNameParts[2]
-			iIndex = None
-			iSuffix = None
-			sSide = getKeyFromNamePart(lNameParts[1], 'side')
-			if sSide:
-				sRes = None 
-				sResValue = None
+			self.sPart = lNameParts[2]
+			self.iIndex = None
+			self.iSuffix = None
+			self._sSide = getKeyFromNamePart(lNameParts[1], 'side')
+			if self._sSide:
+				self._sRes = None 
 			else:
-				sRes = getKeyFromNamePart(lNameParts[1], 'resolution')
-
-		self.sType = sType
-		self.sSide = sSide
-		self.sRes = sRes
-		self.sPart = sPart
-		self.iIndex = iIndex
-		self.iSuffix = iSuffix
-
-	def getKeyFullName(self, sKey, sType):
-		return getFullNameFromKey(sKey, sType)
-
+				self._sRes = getKeyFromNamePart(lNameParts[1], 'resolution')
 
 # functions
 def getKeyFromNamePart(sNamePart, sKeyType):
