5dce3b82b11cf6aa0e07bf8f617778dd59990421
Harris <25195237@qq.com>
2019-04-21 11:16:04 -0700

    wip

---
+++ maya/templates/utils_template.py
@@ -0,0 +1,33 @@
+#=================#
+# IMPORT PACKAGES #
+#=================#
+
+## import system packages
+import sys
+import os
+
+## import python packages
+import math
+import json
+import cPickle
+
+## import compiled packages
+import numpy
+
+## import maya packages
+import maya.cmds as cmds
+import maya.api.OpenMaya as OpenMaya
+
+## import utils
+
+#=================#
+#   GLOBAL VARS   #
+#=================#
+
+#=================#
+#      CLASS      #
+#=================#
+
+#=================#
+#    FUNCTION     #
+#=================#
