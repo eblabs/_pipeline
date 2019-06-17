b2b7900111006d34250f876e7650f70578ded621
Harris <25195237@qq.com>
2019-06-16 22:27:45 -0700

    wip
    wip

--- maya/dev/rigging/builder/core/builder.py
+++ maya/dev/rigging/builder/core/builder.py
@@ -33,7 +33,9 @@ class Builder(object):
 	"""
 	def __init__(self):
 		super(Builder, self).__init__()
-		self._tasks = []
+		self._data = []
+		self._tasks = {}
+		self._hierarchy = []
 		
 	def register_task(self, **kwargs):
 		'''
@@ -56,17 +58,58 @@ class Builder(object):
 		_kwargs = variables.kwargs('kwargs', {}, kwargs)
 
 		# add to task list
-		_taskInfo = {'name': {'task': _task,
-							  'index': _index,
-							  'parent': _parent,
-							  'kwargs': _kwargs}}
-		self._tasks.append(_taskInfo)
+		_taskInfo = {_name: {'task': _task,
+							 'parent': _parent,
+							 'kwargs': _kwargs}}
+
+		# get index
+		if isinstance(_index, basestring):
+			for i, t in enumerate(self._data):
+				key = t.keys()[0]
+				if _index == key:
+					_index = i+1
+					break
+			if isinstance(_index, basestring):
+				_index = len(self._data)
+
+		self._data.insert(_index, _taskInfo)
+		self._tasks.update({_name: {'task': _task,
+									'kwargs': _kwargs}})
 
 	def registertion(self):
 		'''
 		register all the task to the builder
 		'''
 		pass
+
+	def _insert_child(self, tree, data):
+		'''
+		insert child to parent task as a tree hierarchy
+		'''
+		key = data.keys()[0]
+		item = data[key]
+		if 'parent' in item and item['parent']:
+			parentTask = item['parent']
+			for treeData in tree:
+				keyTree = treeData.keys()[0]
+				itemTree = treeData[keyTree]
+				if parentTask == keyTree:
+					itemTree['children'].append({key: {'children': []}})
+					return True
+				else:
+					isParent = get_child(itemTree['children'], data)
+					if isParent:
+						return True
+		else:
+			tree.append({key: {'children': []}})
+
+	def _build_task_tree(self):
+		'''
+		build task tree hierarchy
+		'''
+		for d in self._data:
+			self._insert_child(self._hierarchy, d)
+
 
 
 
