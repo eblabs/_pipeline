cb5da9ea04f914db3f5ebcd34a4d38b00d5e5776
Harris <25195237@qq.com>
2019-08-09 21:18:18 -0700

    wip
    wip

--- maya/dev/rigging/builder/ui/widgets/rigInfo.py
+++ maya/dev/rigging/builder/ui/widgets/rigInfo.py
@@ -34,19 +34,28 @@ class RigInfo(QWidget):
         super(RigInfo, self).__init__()
         self._enable = True
 
+        self.project_check = False
+        self.asset_check = False
+        self.rig_check = False
+
         layout_base = QVBoxLayout()
         self.setLayout(layout_base)
 
-        for section, tip, menu in zip(['project', 'asset', 'rig'],
-                                [TT_PROJECT, TT_ASSET, TT_RIG],
-                                [False, False, True]):
+        for section, tip in zip(['project', 'asset', 'rig'],
+                                [TT_PROJECT, TT_ASSET, TT_RIG]):
             # QLineEdit
-            line_edit_section = LineEdit(name=section, tool_tip=tip, menu=menu)
+            line_edit_section = LineEdit(name=section, tool_tip=tip)
             # add obj to class for further use
             setattr(self, 'line_edit_'+section, line_edit_section)
 
             # add section to base layout
             layout_base.addWidget(line_edit_section)
+
+        # rig right clicked menu
+        self.menu = QMenu()
+        self.create_action = self.menu.addAction('Create')
+        self.remove_action = self.menu.addAction('Remove')
+        self.line_edit_rig.setContextMenuPolicy(Qt.CustomContextMenu)
 
         # get all projects
         projects_list = assets.get_all_projects()
@@ -55,8 +64,15 @@ class RigInfo(QWidget):
         # connect signal
         self.line_edit_project.textChanged.connect(self.assets_completer)
         self.line_edit_project.textChanged.connect(self.check_project)
+
         self.line_edit_asset.textChanged.connect(self.rigs_completer)
+        self.line_edit_asset.textChanged.connect(self.check_asset)
+
+        self.line_edit_rig.textChanged.connect(self.check_rig)
         self.line_edit_rig.customContextMenuRequested.connect(self.show_menu)
+
+        self.create_action.triggered.connect(self.create_rig)
+        self.remove_action.triggered.connect(self.remove_rig)
 
         # so it won't focus on QLineEdit when startup
         self.setFocus()
@@ -90,24 +106,92 @@ class RigInfo(QWidget):
 
     def check_project(self):
         project = self.line_edit_project.text()
-        check = assets.get_project_path(project, warning=False)
+        self.project_check = assets.get_project_path(project, warning=False)
+        self.set_text_color(self.line_edit_project, self.project_check)
+
+    def check_asset(self):
+        asset = self.line_edit_asset.text()
+        project = self.line_edit_project.text()
+        self.asset_check = assets.get_asset_path_from_project(asset, project, warning=False)
+        self.set_text_color(self.line_edit_asset, self.asset_check)
+
+    def check_rig(self):
+        rig = self.line_edit_rig.text()
+        asset = self.line_edit_asset.text()
+        project = self.line_edit_project.text()
+        self.rig_check = assets.get_rig_path_from_asset(rig, asset, project, warning=False)
+        self.set_text_color(self.line_edit_rig, self.rig_check)
+
+    def create_rig(self):
+        rig = self.line_edit_rig.text()
+        asset = self.line_edit_asset.text()
+        project = self.line_edit_project.text()
+
+        title = "Create Rig"
+        text = "This will create rig {} under {} - {}, are you sure you want to create it?".format(rig, asset, project)
+        reply = QMessageBox.warning(self, title, text, QMessageBox.Ok | QMessageBox.Cancel,
+                                    defaultButton=QMessageBox.Cancel)
+
+        if reply == QMessageBox.Ok:
+            # create rig
+            assets.create_rig(rig, asset, project)
+            self.check_rig()
+
+    def remove_rig(self):
+        rig = self.line_edit_rig.text()
+        asset = self.line_edit_asset.text()
+        project = self.line_edit_project.text()
+
+        title = "Remove Rig"
+        text = "This will remove rig {} under {} - {}, are you sure you want to remove it?".format(rig, asset, project)
+        reply = QMessageBox.warning(self, title, text, QMessageBox.Ok | QMessageBox.Cancel,
+                                    defaultButton=QMessageBox.Cancel)
+
+        if reply == QMessageBox.Ok:
+            # create rig
+            assets.remove_rig(rig, asset, project)
+            self.check_rig()
+
+    def show_menu(self, pos):
+        # check if typed rig available
+        if self.rig_check:
+            # disable create, enable remove
+            self.create_action.setEnabled(False)
+            self.remove_action.setEnabled(True)
+        else:
+            # check if text in rig
+            rig = self.line_edit_rig.text()
+            if rig:
+                # enable remove disable create
+                self.create_action.setEnabled(True)
+            else:
+                self.create_action.setEnabled(False)
+            self.remove_action.setEnabled(False)
+
+        pos = self.line_edit_rig.mapToGlobal(pos)
+        self.menu.move(pos)  # move menu to the clicked position
+        self.menu.show()
+
+    @staticmethod
+    def set_text_color(line_edit, check):
+        """
+        set line edit text color, white if available, else is red
+
+        Args:
+            line_edit(QLineEdit): rig info widget
+            check(bool): True/False
+        """
         palette = QPalette()
         if check:
             palette.setColor(QPalette.Text, Qt.white)
-            self.line_edit_project.setPalette(palette)
         else:
             palette.setColor(QPalette.Text, Qt.red)
-            self.line_edit_project.setPalette(palette)
-
-    def show_menu(self, pos):
-        pos = self.line_edit_rig.mapToGlobal(pos)
-        self.line_edit_rig.menu.move(pos)  # move menu to the clicked position
-        self.line_edit_rig.menu.show()
+        line_edit.setPalette(palette)
 
 
 class LineEdit(QLineEdit):
     """lineEdit for each rig info section"""
-    def __init__(self, name='', tool_tip='', menu=False):
+    def __init__(self, name='', tool_tip=''):
         super(LineEdit, self).__init__()
 
         self._name = name
@@ -122,9 +206,3 @@ class LineEdit(QLineEdit):
 
         self.completer = QCompleter()
         self.setCompleter(self.completer)
-
-        if menu:
-            self.menu = QMenu()
-            self.create_action = self.menu.addAction('Create')
-            self.remove_action = self.menu.addAction('Remove')
-            self.setContextMenuPolicy(Qt.CustomContextMenu)
